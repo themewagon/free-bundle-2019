60386790e08ec1d199a7e90fa03a96deb3a36429
Ovi <mhcovi003@gmail.com>
2018-08-08 18:26:19 +0600

    Add back to top button

--- asset/js/script.js
+++ asset/js/script.js
@@ -25,4 +25,20 @@ $(document).ready(function(){
 		$("#dropdownMenuButton").text($(this).text()); 
 	});
 
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 500){
+			$(".back-to-top").css("opacity","1");
+		}
+		else{
+
+			$(".back-to-top").css("opacity","0");
+		}
+	});
+
+	$(".back-to-top").click(function(e){
+		$("html, body").animate({ scrollTop: 0 },"slow");
+
+		return false;
+	});
+
 });
