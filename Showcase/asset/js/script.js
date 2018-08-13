7587a0393b3c4d1c708ba3c789b64542c77c858e
Ovi <mhcovi003@gmail.com>
2018-08-13 10:40:06 +0600

    Update

--- Showcase/asset/js/script.js
+++ Showcase/asset/js/script.js
@@ -1,5 +1,69 @@
 $(document).ready(function(){
 
+	console.log(images);
+	navbar();
+
+	$(window).resize(function(){
+		$(".nav-item").removeAttr("style")
+		$(".category-list").html(' ');
+		navbar();
+	});
+
+	function navbar(){
+		var totalWidth = 0;
+		var navbarWidth = $(".navbar").width() - $(".navbar .dropdown").width();
+
+		$(".nav-item").each(function(){
+
+				totalWidth += $(this).width();
+
+				if(totalWidth > navbarWidth){
+					if(!$(this).hasClass("dropdown")){
+						$(this).hide();
+						var link = $(this).html();
+
+						console.log(link);
+
+						$(".category-list").append(link);
+					}
+				}
+		});
+
+		$(".dropdown-menu .nav-link").each(function(){
+			$(this).removeClass("nav-link").addClass("dropdown-item");
+		});
+	}
+
+	$(window).scroll(function(){
+		if($(window).scrollTop() > 540){
+			$(".back-to-top").css("opacity","1");
+			$(".navbar").addClass("sticky-top");
+		}
+		else{
+
+			$(".back-to-top").css("opacity","0");
+			$(".navbar").removeClass("fixed");
+		}
+	});
+
+	$(".back-to-top").click(function(e){
+		$("html, body").animate({ scrollTop: 0 },"slow");
+
+		return false;
+	});
+
+	$(document).on("click",".nav-link, .dropdown-item",function(e){
+		console.log(e.target);
+
+		$(".nav-link").removeClass("active");
+		$(".dropdown-item").removeClass("active");
+
+		$(this).addClass("active");
+	});
+
+
+}), $(window).on("load",function(){
+
 	var $grid = $('.grid').masonry({
 	  	// options...
 	  	itemSelector: '.item',
@@ -7,12 +71,11 @@ $(document).ready(function(){
 	});
 
 	// filter functions
-	var filterFns = {
-
-	};
+	var filterFns = {};
 
 	// bind filter button click
-	$('.category-list').on( 'click', 'a', function(e) {
+	$('.navbar').on( 'click', 'a', function(e) {
+
 		e.preventDefault();
 		
 		var filterValue = $( this ).attr('data-filter');
@@ -20,9 +83,5 @@ $(document).ready(function(){
 		filterValue = filterFns[ filterValue ] || filterValue;
 		$grid.isotope({ filter: filterValue });
 	});
-
-	$(".category-list > a").click(function() {
-		$("#dropdownMenuButton").text($(this).text()); 
-	});
-
+	
 });
